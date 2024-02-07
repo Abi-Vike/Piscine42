00e29fd7e737d4c3eee7df91f28a1544dfb2cf32
ayacob <ayacob@student.42berlin.de>
2024-02-07 14:52:25 +0100

    solved ex06 and also 7 & 8

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,22 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayacob <ayacob@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 00:55:19 by ayacob            #+#    #+#             */
-/*   Updated: 2024/02/07 13:09:04 by ayacob           ###   ########.fr       */
+/*   Created: 2024/02/07 14:32:31 by ayacob            #+#    #+#             */
+/*   Updated: 2024/02/07 14:46:25 by ayacob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strlen(char *str)
-{
-    int count = 0;
-    while (*str != '\0')
-    {
-        count++;
-        str++;
+
+void ft_rev_int_tab(int *tab, int size) {
+    int start = 0;
+    int end = size - 1;
+    while (start < end) {
+        int temp = tab[start];
+        tab[start] = tab[end];
+        tab[end] = temp;
+        start++;
+        end--;
     }
-    return count;
 }
--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,22 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayacob <ayacob@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/07 00:55:19 by ayacob            #+#    #+#             */
-/*   Updated: 2024/02/07 13:09:04 by ayacob           ###   ########.fr       */
+/*   Created: 2024/02/07 14:48:33 by ayacob            #+#    #+#             */
+/*   Updated: 2024/02/07 14:51:34 by ayacob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strlen(char *str)
-{
-    int count = 0;
-    while (*str != '\0')
-    {
-        count++;
-        str++;
+void ft_sort_int_tab(int *tab, int size) {
+    // using bubble sort algorithm
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (tab[j] > tab[j + 1]) {
+                // need to Swap elements if they are in the wrong order
+                int temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+        }
     }
-    return count;
 }
